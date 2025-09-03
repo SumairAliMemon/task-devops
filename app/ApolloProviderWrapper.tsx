a92203137305af907f8d964f51e9e9207b4710dd
SumairAliMemon <sumairmemon897@gmail.com>
2025-09-03 16:36:00 +0500

    push final code

--- app/ApolloProviderWrapper.tsx
+++
@@ -1,4 +0,0 @@
-// ApolloProviderWrapper is no longer needed with ApolloNextAppProvider in layout.tsx
-export default function ApolloProviderWrapper({ children }: { children: React.ReactNode }) {
-  return children;
-}
